bc5e75e98269c9c155d26724de8e0ea7dcb58b20
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2022-12-28 08:12:40 +0100

    refactor(Home): changed the layout and style of the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,19 @@
-import { PageTemplate } from "../../components";
+import { PageTemplate, OutlineButton } from "../../components";
+import { useNavigate } from "react-router-dom";
 import "./home.css";
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
     <PageTemplate>
       <section className="pageSection homePageSection">
         <div className="homeTextContainer">
-          <h1 className="pageHeader1">Emmanuel Jemeni</h1>
-          <h2 className="textColorTickleMePink">Front End Web Developer</h2>
-          <p>I create wonders on the web</p>
-        </div>
-        <div className="animationContainer">
-          <p>placeholder text</p>
+          <h1 className="title">Emmanuel Jemeni</h1>
+          <h2 className="subTitle textColorTickleMePink">
+            Front-End Web Developer
+          </h2>
+          <OutlineButton text="About Me" onClick={() => navigate("/about")} />
         </div>
       </section>
     </PageTemplate>
